3822779192410f569ea7d9ae942543e9aa4b2d61
mattbrailsford <me@mattbrailsford.com>
2018-03-15 21:28:31 +0000

    Modal styles Reward names Now using jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
-(function () {
+$(function(){
 
     // Configure tabs
-    var tabTabs = document.getElementsByClassName('tabs__tab');
-    var tabContents = document.getElementsByClassName('tabs__content');
+    $('.tabs__tab a').on('click', function (e) {
+        e.preventDefault();
+        $('.tabs__content').removeClass('tabs__content--active');
+        $($(this).attr('href')).addClass('tabs__content--active');
+        $('.tabs__tab').removeClass('tabs__tab--active');
+        $(this).closest('.tabs__tab').addClass('tabs__tab--active');
+    })
 
-    [].forEach.call(tabTabs, function(tab) {
-        var a = tab.getElementsByTagName('a')[0];
-        a.addEventListener('click', function (e){
-            e.preventDefault();
+    // Show pledge dialog
+    $('.btn--pledge:not([disabled]), .reward:not([disabled])').on('click', function (e) {
+        e.preventDefault();
+        var reward = $(this).data('reward');
+        $('select[name=reward]').val(reward);
+        $('html').addClass('no-scroll');
+        $('.modal').show();
+    })
 
-            // Toggle content
-            [].forEach.call(tabContents, function(t) {
-                t.classList.remove('tabs__content--active')
-            });
-            document.getElementById(this.getAttribute('href').substr(1)).classList.add('tabs__content--active');
+    // Hide pledge dialog
+    $('.modal__cancel').on('click', function (e) {
+        e.preventDefault();
+        $('html').removeClass('no-scroll');
+        $('.modal').hide();
+    })
 
-            // Toggle tabs
-            [].forEach.call(tabTabs, function(t) {
-                t.classList.remove('tabs__tab--active')
-            });
-            this.parentElement.classList.add('tabs__tab--active')
-
-        })
-    });
-
-})();
+});
